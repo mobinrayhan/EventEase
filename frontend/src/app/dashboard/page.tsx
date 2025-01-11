088b882086153cee64b82e1141aeb9f2b62f3407
Rayhan Uddin Mobin <158156914+mobinrayhan@users.noreply.github.com>
2025-01-12 01:56:09 +0600

    added my events at dashboard page

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,3 +1,22 @@
+import EventList from "../../components/events/event-list";
+
+const myEvents = [
+  {
+    id: 2,
+    name: "Music Fest",
+    date: "2025-03-10",
+    location: "Chittagong, Bangladesh",
+    maxAttendees: 500,
+    registeredAttendees: 450,
+    createdBy: "Alex Johnson",
+  },
+];
+
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <div className="p-6">
+      <h1 className="mb-6 text-center text-2xl font-bold">My Events</h1>
+      <EventList events={myEvents} />
+    </div>
+  );
 }
