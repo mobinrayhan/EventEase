c6247ec3a277f2d02c175464cde3478b6910f4f6
Rayhan Uddin Mobin <158156914+mobinrayhan@users.noreply.github.com>
2025-01-12 00:32:03 +0600

    added prettier auto format for tailwind class

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,3 @@
-import Button from "../components/UI/button";
-import Input from "../components/UI/input";
-
 export default function Home() {
-  return (
-    <div>
-      <Button>Click me</Button>
-      <Input label="My Name" id="myName" direction="flex-col" />
-    </div>
-  );
+  return <div></div>;
 }
